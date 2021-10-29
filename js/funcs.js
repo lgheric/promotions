629a4c1ea9225a0792c580d07017b4aa204646b8
lgheric <saverneo@163.com>
2021-10-29 19:26:39 +0800

    add Travelocity

--- js/funcs.js
+++ js/funcs.js
@@ -1,19 +1,24 @@
 //
 flighthub = "https://www.flighthub.com/?campaign=371&utm_campaign=Yazing&utm_content=2179451-2639103&utm_medium=affiliate&utm_source=cj&cjevent=1ce6b21c357311ec83ab75600a1c0e12";
+travelocity = "https://yazing.com/deals/travelocity/saverneo";
 
-$(function () {         //当文档加载完毕自动执行此处代码
+$(function () {
 
-    $("img").click(function () {        //绑定事件（方法一）
+    $("img").click(function () {
         //$(this)表示当前点击的标签
         switch($(this).attr("data-target")){
-            case "flighthub" : window.location.href= flighthub;
+            case "flighthub" : window.location.href= flighthub;break;
+            case "travelocity" : window.location.href= travelocity;break;
+            default:break;
         }
     });
 
-    $("span").click(function () {        //绑定事件（方法一）
+    $("span").click(function () {
         //$(this)表示当前点击的标签
         switch($(this).attr("data-target")){
-            case "flighthub" : window.location.href= flighthub;
+            case "flighthub" : window.location.href= flighthub;break;
+            case "travelocity" : window.location.href= travelocity;break;
+            default:break;
         }
     });
 
