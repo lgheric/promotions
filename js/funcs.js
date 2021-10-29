39981840d07b424707b7f7658cd9925e70818f0b
lgheric <saverneo@163.com>
2021-10-29 21:28:14 +0800

    add clear

--- js/funcs.js
+++ js/funcs.js
@@ -1,6 +1,7 @@
 //
 flighthub = "https://www.flighthub.com/?campaign=371&utm_campaign=Yazing&utm_content=2179451-2639103&utm_medium=affiliate&utm_source=cj&cjevent=1ce6b21c357311ec83ab75600a1c0e12";
 travelocity = "https://yazing.com/deals/travelocity/saverneo";
+clearme = "https://yazing.com/deals/clearme/saverneo";
 
 $(function () {
 
@@ -9,6 +10,7 @@ $(function () {
         switch($(this).attr("data-target")){
             case "flighthub" : window.location.href= flighthub;break;
             case "travelocity" : window.location.href= travelocity;break;
+            case "clearme" : window.location.href= clearme;break;
             default:break;
         }
     });
@@ -18,6 +20,7 @@ $(function () {
         switch($(this).attr("data-target")){
             case "flighthub" : window.location.href= flighthub;break;
             case "travelocity" : window.location.href= travelocity;break;
+            case "clearme" : window.location.href= clearme;break;
             default:break;
         }
     });
