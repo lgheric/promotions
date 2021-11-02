deffe21fa78408071d0520e1f9b30c6c70356cfd
lgheric <saverneo@163.com>
2021-11-02 12:08:07 +0800

    update the url of airportlax

--- js/funcs.js
+++ js/funcs.js
@@ -5,7 +5,7 @@ clearme = "https://yazing.com/deals/clearme/saverneo";
 rvshare = "https://yazing.com/deals/rvshare/saverneo";
 adrenaline = "https://yazing.com/go/33961872/saverneo";
 flyingblue = "https://yazing.com/go/19565839/saverneo";
-airportlax = "https://yazing.com/go/19565839/saverneo";
+airportlax = "https://yazing.com/go/41770163/saverneo";
 
 $(function () {
 
